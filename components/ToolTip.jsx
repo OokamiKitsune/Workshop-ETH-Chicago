d0ff27668d9072b3bf120939d69601eb4850fa24
tippi fifestarr <tippi@boober.org>
2023-09-24 08:54:22 -0500

    added tippi and alignment

--- components/ToolTip.jsx
+++ components/ToolTip.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import tooltipIcon from "../public/images/tooltip-icon.png";
 import Tippy from "@tippyjs/react";
 import InfoIcon from "./InfoIcon";
+import React from "react";
 
 Tippy.defaultProps = {
   placement: "top",
@@ -12,19 +13,39 @@ Tippy.defaultProps = {
   aria: "describedby",
 };
 
-export default function Tooltip({ content, children, defaultContainerClass: setCustomStyle, ...props }) {
+export default function Tooltip({
+  content,
+  children,
+  defaultContainerClass: setCustomStyle,
+  ...props
+}) {
   return (
     <Tippy
       content={
-        <div className="oswald flex flex-row items-center text-left max-w-xl"> {/* Set a max width here */}
-          <Image className="mr-4 h-8" src={tooltipIcon} width={25} height={25} alt="Tip" />
-          <div className="max-w-xs break-words">{content}</div> {/* Constrain content width */}
+        <div className="oswald flex flex-row items-center text-left max-w-xl">
+          <Image
+            className="mr-4 h-8"
+            src={tooltipIcon}
+            width={25}
+            height={25}
+            alt="Tip"
+          />
+          <div className="max-w-xs break-words">
+            {content.split("\n").map((text, index, array) => (
+              <React.Fragment key={index}>
+                {text}
+                {/* Add a line break for all except the last */}
+                {index === array.length - 1 ? null : <br />}
+              </React.Fragment>
+            ))}
+          </div>
         </div>
       }
       {...props}
     >
-
-      <div className={`absolute top-3 right-6 flex items-center justify-center w-8 h-8 rounded-full bg-purple-700 ${setCustomStyle}`}>
+      <div
+        className={`absolute top-3 right-6 flex items-center justify-center w-8 h-8 rounded-full bg-purple-700 ${setCustomStyle}`}
+      >
         <InfoIcon />
         {children}
       </div>
--- pages/quests/tool-tip-quest-solution.tsx
+++ pages/quests/tool-tip-quest-solution.tsx
@@ -3,27 +3,27 @@ import ToolTipBox from "../../components/TechQuests/ToolTipBox";
 import developerGandalf from "../../components/developers/developerTemplate";
 
 // Welcome to the tool-tip-quest! This quest will teach you how to create a tooltip using React components.
-// We have provided you with most of the code you need to get started. 
+// We have provided you with most of the code you need to get started.
 // For context: We have created our own component called ToolTipBox that will be used to display some basic information on the DOM.
-// Before you implement your own code, you will need to create a developer object and import it to this file. Simply adding a string is not good practice. 
+// Before you implement your own code, you will need to create a developer object and import it to this file. Simply adding a string is not good practice.
 // These objects are located in the components/developers folder.
 
-
 export default function ToolTipQuest() {
-
-    return (
+  return (
     <>
-        <div className="flex items-center justify-center mt-10 mb-10">
-            <div className="flex flex-col mb-5">
-                <h1 className="text-2xl font-semibold text-center">To begin, open this page in your IDE: /pages/quests/tool-tip-quest</h1>
-                {/* Use the ToolTipBox component and pass developer-specific data as props */}
-                <ToolTipBox name={developerGandalf.name} characterClass={developerGandalf.characterClass} />
-
-
-
-            </div>
+      <div className="flex items-center justify-center mt-10 mb-10">
+        <div className="flex flex-col mb-5">
+          <h1 className="text-2xl font-semibold text-center">
+            To begin, open this page in your IDE: /pages/quests/tool-tip-quest
+          </h1>
+          {/* Use the ToolTipBox component and pass developer-specific data as props */}
+          <ToolTipBox
+            name={developerGandalf.name}
+            characterClass={developerGandalf.characterClass}
+            alignment="very good"
+          />
         </div>
-    
+      </div>
     </>
   );
 }
